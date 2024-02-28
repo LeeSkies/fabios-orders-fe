0f9df03de4603a11ab615b86eed475a465f802b6
leeskies <lee2dskies@gmail.com>
2024-02-28 21:23:22 +0200

    updated tsconfig

--- src/features/common/forms/DetailsForm.tsx
+++ src/features/common/forms/DetailsForm.tsx
@@ -9,24 +9,18 @@ type Props = {
   details: OrderDetails;
 };
 
-export const DetailsForm = ({ order, details }: Props) => {
-  const [dto, setDto] = useState(new OrderFormData());
+export const DetailsForm = ({ order }: Props) => {
+  // const [dto, setDto] = useState(new OrderFormData());
   return (
     <form className="bg-slate-100 p-8">
       <div className="flex justify-around">
-        <button
-          className={`${order.status == "P" && "p-px bg-green-500"}`}
-        >
+        <button className={`${order.status == "P" && "p-px bg-green-500"}`}>
           <Status status="P" />
         </button>
-        <button
-          className={`${order.status == "AP" && "p-px bg-green-500"}`}
-        >
+        <button className={`${order.status == "AP" && "p-px bg-green-500"}`}>
           <Status status="AP" />
         </button>
-        <button
-          className={`${order.status == "D" && "p-px bg-green-500"}`}
-        >
+        <button className={`${order.status == "D" && "p-px bg-green-500"}`}>
           <Status status="D" />
         </button>
       </div>
